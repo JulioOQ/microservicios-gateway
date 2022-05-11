f5636511435ef2eba7f1ee86bee7cedabb3f9102
Julio Vladimir Ore Quispe <jorequis@emeal.nttdata.com>
2022-05-11 15:45:44 -0500

    gateway v1 by jvoq

---
+++ src/test/java/com/jvoq/microservicios/gateway/app/MicroserviciosGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvoq.microservicios.gateway.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
